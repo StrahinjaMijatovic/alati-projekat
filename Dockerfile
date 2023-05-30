aafd31aefc62560f7a215b6876b72b7bbf144602
Strahinja <Strahinja@LAPTOP-13HLLGCJ>
2023-05-30 12:33:20 +0200

    Popravke

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,27 @@
+#Build Stage
+
 FROM golang:latest as builder
 
-WORKDIR /app
+WORKDIR /ars-2023
 
 COPY go.mod go.sum ./
 
 RUN go mod download
 
 COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
 
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o server .
-
+#Run Stage
 FROM alpine:latest
 
 RUN apk --no-cache add ca-certificates
 
 WORKDIR /root/
 
-COPY --from=builder /app/server .
-
 EXPOSE 8000
 
-CMD ["./server"]
+COPY --from=builder /ars-2023/main .
+
+COPY ./swagger.yaml .
+
+CMD ["./main"]
--- main.go
+++ main.go
@@ -1,50 +1,76 @@
+// Config Service API
+//
+//	   Title: Config Service API
+//
+//	   Schemes: http
+//		  Version: 0.0.1
+//		  BasePath: /
+//
+//		  Produces:
+//			- application/json
+//
+// swagger:meta
 package main
 
 import (
 	"context"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
-
-	"github.com/gorilla/mux"
 )
 
-type Config struct {
-	Entries map[string]string `json:"entries"`
-}
-
-type Service struct {
-	data map[string][]*Config `json:"data"`
-}
-
-func NewService() *Service {
-	return &Service{
-		data: make(map[string][]*Config),
-	}
+type dbServerConfig struct {
+	data      map[string]*DBConfig
+	dataGroup map[string]*DBGroup
 }
 
 func main() {
+
 	quit := make(chan os.Signal)
-	service := NewService()
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
 
 	router := mux.NewRouter()
 	router.StrictSlash(true)
 
-	router.HandleFunc("/config/{id}", service.AddConfig).Methods("POST")
-	router.HandleFunc("/config-group/{id}", service.AddConfigGroup).Methods("POST")
-	router.HandleFunc("/config/{id}", service.GetConfig).Methods("GET")
-	router.HandleFunc("/config-group/{id}", service.GetConfigGroup).Methods("GET")
-	router.HandleFunc("/config/{id}", service.DeleteConfig).Methods("DELETE")
-	router.HandleFunc("/config-group/{id}", service.DeleteConfigGroup).Methods("DELETE")
-	router.HandleFunc("/config-group/{id}", service.AddConfigToGroup).Methods("PUT")
+	server := dbServerConfig{
+		data:      map[string]*DBConfig{},
+		dataGroup: map[string]*DBGroup{},
+	}
 
-	//log.Fatal(http.ListenAndServe(":8080", router))
+	router.HandleFunc("/config/", server.createConfigHandler).Methods("POST")
+	router.HandleFunc("/configs/", server.getAllConfigHandler).Methods("GET")
+	router.HandleFunc("/configs/{id}/", server.getConfigVersionsHandler).Methods("GET")
+	router.HandleFunc("/configs/{id}/", server.delConfigVersionsHandler).Methods("DELETE")
+	router.HandleFunc("/config/{id}/{version}/", server.getConfigHandler).Methods("GET")
+	router.HandleFunc("/config/{id}/{version}/", server.delConfigHandler).Methods("DELETE")
+
+	router.HandleFunc("/group/", server.createGroupHandler).Methods("POST")
+	router.HandleFunc("/groups/", server.getAllGroupHandler).Methods("GET")
+	router.HandleFunc("/groups/{id}/", server.getGroupVersionsHandler).Methods("GET")
+	router.HandleFunc("/groups/{id}/", server.delGroupVersionsHandler).Methods("DELETE")
+	router.HandleFunc("/group/{id}/{version}/", server.getGroupHandler).Methods("GET")
+	router.HandleFunc("/group/{id}/{version}/", server.delGroupHandler).Methods("DELETE")
+	router.HandleFunc("/group/{id}/{version}/{new}/", server.appendGroupHandler).Methods("POST")
+
+	router.HandleFunc("/group/{id}/{version}/{labels}/", server.getConfigsByLabel).Methods("GET")
+	router.HandleFunc("/group/{id}/{version}/{new}/{labels}/", server.delConfigsByLabel).Methods("DELETE")
+
+	router.HandleFunc("/swagger.yaml", server.swaggerHandler).Methods("GET")
+
+	// SwaggerUI
+	optionsDevelopers := middleware.SwaggerUIOpts{SpecURL: "swagger.yaml"}
+	developerDocumentationHandler := middleware.SwaggerUI(optionsDevelopers, nil)
+	router.Handle("/docs", developerDocumentationHandler)
 
 	// start server
-	srv := &http.Server{Addr: "0.0.0.0:8080", Handler: router}
+	srv := &http.Server{Addr: "0.0.0.0:8000", Handler: router}
 	go func() {
-		log.Println("server starting")
+		log.Println("...server starting...")
 		if err := srv.ListenAndServe(); err != nil {
 			if err != http.ErrServerClosed {
 				log.Fatal(err)
@@ -54,22 +80,15 @@ func main() {
 
 	<-quit
 
-	log.Println("service shutting down ...")
+	log.Println("...server shutting down...")
 
-	// gracefully stop server
+	//graceful shutdown
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal(err)
 	}
-	log.Println("server stopped")
+	log.Println("...server stopped...")
 
 }
-
-//{
-//"entries": {
-//"key1": "value1",
-//"key2": "value2"
-//}
-//}
